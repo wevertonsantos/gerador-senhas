9f3ba3c015f52550ab501fed1645c769c28d8fb8
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-04 10:44:53 -0300

    letras maiusculas aleatorias

--- gerador.py
+++ gerador.py
@@ -7,7 +7,7 @@ Gerador de senhas
 '''
 
 # 1 - pedir ao usuário o tamanho da senha
-tamanho_senha = 12 #input("Digite o tamanho da senha: ")
+tamanho_senha = 12 #int(input("Digite o tamanho da senha: "))
 #2 - criar a senha do tamanho pedido pelo usuário
 i = 0
 senha = ""
@@ -15,3 +15,7 @@ while i < tamanho_senha:
     senha += str(i)
     i += 1
 print(senha)
+# 3 - letras maiusculas aleatorias na senha
+lista_letras_maiusculas = ["A","W","C","G","D","P","L","K","D","S"]
+letra_maiuscula = random.choice(lista_letras_maiusculas)
+print(letra_maiuscula)
