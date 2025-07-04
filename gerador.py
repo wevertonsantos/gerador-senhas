5b8640756eb07753bdababbdbfc70043104e0a25
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-04 11:22:43 -0300

    adicionando letras minusculas aleatorias na senha

--- gerador.py
+++ gerador.py
@@ -8,10 +8,14 @@ Gerador de senhas
 
 # 1 - pedir ao usuário o tamanho da senha
 tamanho_senha = 12 #int(input("Digite o tamanho da senha: "))
+
 #2 - criar a senha do tamanho pedido pelo usuário
+
 i = 0
 senha = ""
 lista_letras_maiusculas = ["A","W","C","G","D","P","L","K","D","S"]
+lista_letras_minusculas = ["a","b","d","e","s","w","s","x","z","y"]
+
 #while construindo a senha
 while i < tamanho_senha:
     # se tamanho da senha for igual tamanho passado pelo usuário
@@ -22,5 +26,7 @@ while i < tamanho_senha:
         senha += str(random.randint(0,9))
         # 4 - letras maiusculas aleatorias na senha
         senha += random.choice(lista_letras_maiusculas)
+        # 5 - letras minusculas aleatorias na senha
+        senha += random.choice(lista_letras_minusculas)
     i += 1
 print(senha)
