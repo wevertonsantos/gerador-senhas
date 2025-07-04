0be764f9842c04d2c1d25dd170d013641ee1b1ef
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-04 11:44:36 -0300

    restruturando código e tentando criar senha do tamanho pedido pelo usuário

--- gerador.py
+++ gerador.py
@@ -7,26 +7,24 @@ Gerador de senhas
 '''
 
 # 1 - pedir ao usuário o tamanho da senha
-tamanho_senha = 12 #int(input("Digite o tamanho da senha: "))
-
-#2 - criar a senha do tamanho pedido pelo usuário
+tamanho_senha = 10 #int(input("Digite o tamanho da senha: "))
 
 i = 0
 senha = ""
 lista_letras_maiusculas = ["A","W","C","G","D","P","L","K","D","S"]
 lista_letras_minusculas = ["a","b","d","e","s","w","s","x","z","y"]
+lista_simbolos = ["!","@","$","%","&","-","*","+"]
 
-#while construindo a senha
+#2 - criar a senha do tamanho pedido pelo usuário
 while i < tamanho_senha:
-    # se tamanho da senha for igual tamanho passado pelo usuário
-    if len(senha) == tamanho_senha:
-        break
-    else:
-        # 3 - adicionar numeros aleatorios na senha
-        senha += str(random.randint(0,9))
-        # 4 - letras maiusculas aleatorias na senha
-        senha += random.choice(lista_letras_maiusculas)
-        # 5 - letras minusculas aleatorias na senha
-        senha += random.choice(lista_letras_minusculas)
     i += 1
+
+# 3 - adicionar numeros aleatorios na senha
+senha += str(random.randint(0,9))
+# 4 - letras maiusculas aleatorias na senha
+senha += random.choice(lista_letras_maiusculas)
+# 5 - letras minusculas aleatorias na senha
+senha += random.choice(lista_letras_minusculas)
+# 6 - simbolos aleatorios na senha
+senha += random.choice(lista_simbolos)
 print(senha)
