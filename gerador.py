58b54a06b9aaf8b5f5553a2dee8976dc4d7c167b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-04 08:58:39 -0300

    Pedindo ao usuário o tamanho da senha e criando função senha

---
+++ gerador.py
@@ -0,0 +1,13 @@
+'''
+Gerador de senhas
+
+1 - Usuário deve escolher tamanho da senha
+2 - Quais caracteres deve ter na senha
+'''
+
+def main():
+    print("Gerador de senhas")
+    tamanho_senha = int(input("Digite o tamanho da senha: "))
+
+def criando_senha(tamanho_senha):
+    ...
