d5a8766f5e6b377d381e038af4dfe9ee8e789ef2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-05 11:21:19 -0300

    agrupando os caracteres em um array completo

--- gerador.py
+++ gerador.py
@@ -16,14 +16,11 @@ incluir_simbolos = "s" #input("Incluir símbolos?? Digite 's' ou 'n'")
 i = 0
 x = 0
 senha = ""
-lista_letras_maiusculas = ["A","W","C","G","D","P","L","K","D","S"]
-lista_letras_minusculas = ["a","b","d","e","s","w","s","x","z","y"]
-lista_simbolos = ["!","@","$","%","&","-","*","+"]
+lista_caracteres = ["A","W","C","G","D","P","L","K","D","S","a","b","d","e","s","w","s","x","z","y","!","@","$","%","&","-","*","+","0","1","2","3","4","5","6","7","8","9"]
 
 #2 - criar a senha do tamanho pedido pelo usuário
-while i < tamanho_senha:
-    senha += random.choice(lista_letras_maiusculas) + random.choice(lista_letras_minusculas) + str(random.randint(0,9)) + random.choice(lista_simbolos)
-    i += 1
+for i in range(tamanho_senha):
+    senha += random.choice(lista_caracteres)
 
     #senha.append(random.choice(lista_letras_maiusculas))
     #senha.append(random.choice(lista_letras_minusculas))
@@ -31,16 +28,3 @@ while i < tamanho_senha:
     #senha.append(random.choice(lista_simbolos))
 # preciso fazer com que cada caractere entre no array e preencha ele por completo
 print(senha)
-
-# 3 - adicionar numeros aleatorios na senha
-if incluir_numeros == 's':
-    senha += str(random.randint(0,9))
-# 4 - letras maiusculas aleatorias na senha
-if incluir_numeros == 's':
-    senha += random.choice(lista_letras_maiusculas)
-# 5 - letras minusculas aleatorias na senha
-if incluir_numeros == 's':
-    senha += random.choice(lista_letras_minusculas)
-# 6 - simbolos aleatorios na senha
-if incluir_numeros == 's':
-    senha += random.choice(lista_simbolos)
